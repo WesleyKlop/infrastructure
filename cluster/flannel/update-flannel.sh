b858748c39755be191f2ec8a2a6579bdbc211856
Wesley Klop <wesley19097@gmail.com>
2021-12-13 17:30:43 +0100

    Upgrade cluster to v1.23.0-00
    * Use ghcr instead of docker.pkg.github.com images
    * Fix plex deployment
    * Fix and improve kube flannel upgrade script

--- cluster/flannel/update-flannel.sh
+++ cluster/flannel/update-flannel.sh
@@ -2,21 +2,20 @@
 
 set -euo pipefail
 
-curl -Lo kube-flannel-new.yml "https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml"
+curl -Lo kube-flannel.yml "https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml"
 
-if cmp -s "kube-flannel.yml" "kube-flannel-new.yml"; then
+if git diff --exit-code kube-flannel.yml; then
     >&2 echo "Files are identical, so there is nothing to do."
     exit 0
 fi
 
 echo -e "Downloaded new version, here is the diff:\n\n\n"
 
-diff kube-flannel.yml kube-flannel-new.yml || true
+git diff kube-flannel.yml
 
 read -p "Keep the new version? " -n 1 -r
-echo    # (optional) move to a new line
+echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    mv kube-flannel-new.yml kube-flannel.yml
     echo "To apply: "
     echo "kubectl apply -f kube-flannel.yml"
 fi
