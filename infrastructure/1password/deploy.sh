b2d5c46d26a99320f15ec1130935ef90745f921b
Wesley Klop <wesley19097@gmail.com>
2022-01-04 01:36:59 +0100

    Upgrade to 1password connect 1.7

--- infrastructure/1password/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-set -eu
-
-exec helm install -n 1password connect 1password/connect \
-  --set-file connect.credentials=1password-credentials.json \
-  --set-file operator.token.value=access-token \
-  --set operator.create=true
