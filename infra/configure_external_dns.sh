5a431838741423a29c506f2ebd8cbc046c598ab0
Wesley Klop <wesley19097@gmail.com>
2022-07-24 11:05:17 +0200

    Add ingress

--- infra/configure_external_dns.sh
+++ infra/configure_external_dns.sh
@@ -11,9 +11,11 @@ GKE_REGION="europe-west4"
 DNS_SA_NAME="external-dns-sa"
 DNS_SA_EMAIL="$DNS_SA_NAME@${GKE_PROJECT_ID}.iam.gserviceaccount.com"
 
-gcloud iam service-accounts create $DNS_SA_NAME --display-name $DNS_SA_NAME
-gcloud projects add-iam-policy-binding $DNS_PROJECT_ID \
-   --member serviceAccount:$DNS_SA_EMAIL --role "roles/dns.admin"
+EXTERNALDNS_NS="external-dns"
+
+# gcloud iam service-accounts create $DNS_SA_NAME --display-name $DNS_SA_NAME
+# gcloud projects add-iam-policy-binding $DNS_PROJECT_ID \
+  #  --member serviceAccount:$DNS_SA_EMAIL --role "roles/dns.admin"
 
 gcloud iam service-accounts add-iam-policy-binding $DNS_SA_EMAIL \
   --role "roles/iam.workloadIdentityUser" \
