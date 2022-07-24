94e8a637b883f90b02027919f545e2b06dc0a248
Wesley Klop <wesley19097@gmail.com>
2022-07-24 11:39:07 +0200

    Fix infra script

--- infra/configure_external_dns.sh
+++ infra/configure_external_dns.sh
@@ -13,10 +13,10 @@ DNS_SA_EMAIL="$DNS_SA_NAME@${GKE_PROJECT_ID}.iam.gserviceaccount.com"
 
 EXTERNALDNS_NS="external-dns"
 
-# gcloud iam service-accounts create $DNS_SA_NAME --display-name $DNS_SA_NAME
-# gcloud projects add-iam-policy-binding $DNS_PROJECT_ID \
-  #  --member serviceAccount:$DNS_SA_EMAIL --role "roles/dns.admin"
+gcloud iam service-accounts create $DNS_SA_NAME --display-name $DNS_SA_NAME
+gcloud projects add-iam-policy-binding $DNS_PROJECT_ID \
+   --member serviceAccount:$DNS_SA_EMAIL --role "roles/dns.admin"
 
 gcloud iam service-accounts add-iam-policy-binding $DNS_SA_EMAIL \
   --role "roles/iam.workloadIdentityUser" \
-  --member "serviceAccount:$GKE_PROJECT_ID.svc.id.goog[${EXTERNALDNS_NS:-"default"}/external-dns]"
+  --member "serviceAccount:$GKE_PROJECT_ID.svc.id.goog[${EXTERNALDNS_NS}/external-dns]"
