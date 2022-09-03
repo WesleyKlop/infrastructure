d4493bdf22b23e73267ce9dead02e78f08230f70
Wesley Klop <wesley19097@gmail.com>
2022-09-03 17:57:14 +0200

    Inline firewall rule config

--- infra/networking.tf
+++ infra/networking.tf
@@ -15,19 +15,132 @@ resource "hcloud_placement_group" "homelab" {
   type = "spread"
 }
 
+# hcloud_firewall.homelab:
 resource "hcloud_firewall" "homelab" {
   name = "homelab"
 
-  dynamic "rule" {
-    for_each = local.firewall_rules
-    content {
-      direction       = rule.value.direction
-      protocol        = rule.value.protocol
-      port            = lookup(rule.value, "port", null)
-      destination_ips = lookup(rule.value, "destination_ips", [])
-      source_ips      = lookup(rule.value, "source_ips", [])
-      description     = lookup(rule.value, "description", null)
-    }
+  # Allowing internal cluster traffic and Hetzner metadata service and cloud API IPs
+  rule {
+    direction  = "in"
+    port       = "any"
+    protocol   = "tcp"
+    source_ips = local.whitelisted_ips
+  }
+  rule {
+    direction  = "in"
+    port       = "any"
+    protocol   = "udp"
+    source_ips = local.whitelisted_ips
+  }
+  rule {
+    direction  = "in"
+    protocol   = "icmp"
+    source_ips = local.whitelisted_ips
+  }
+
+  rule {
+    description = "Allow all traffic to the kube api server"
+    direction   = "in"
+    port        = "6443"
+    protocol    = "tcp"
+    source_ips = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  rule {
+    description = "Allow all traffic towards ssh"
+    direction   = "in"
+    port        = "22"
+    protocol    = "tcp"
+    source_ips = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  rule {
+    description = "Allow ping on ipv4"
+    direction   = "in"
+    protocol    = "icmp"
+    source_ips = [
+      "0.0.0.0/0",
+    ]
+  }
+  rule {
+    description = "ICMP to ping outside services"
+    direction   = "out"
+    protocol    = "icmp"
+    destination_ips = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  rule {
+    description = "DNS"
+    direction   = "out"
+    port        = "53"
+    protocol    = "tcp"
+    destination_ips = [
+      "0.0.0.0/0",
+    ]
+  }
+  rule {
+    description = "DNS"
+    direction   = "out"
+    port        = "53"
+    protocol    = "udp"
+    destination_ips = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  rule {
+    description = "NTP"
+    direction   = "out"
+    port        = "123"
+    protocol    = "udp"
+    destination_ips = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  rule {
+    description = "GPG Keyservers"
+    direction   = "out"
+    port        = "11371"
+    protocol    = "tcp"
+    destination_ips = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  rule {
+    description = "HTTP"
+    direction   = "out"
+    port        = "80"
+    protocol    = "tcp"
+    destination_ips = [
+      "0.0.0.0/0",
+    ]
+  }
+  rule {
+    description = "HTTPS"
+    direction   = "out"
+    port        = "443"
+    protocol    = "tcp"
+    destination_ips = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  rule {
+    description = "Argo CD clone git repositories over ssh"
+    direction   = "out"
+    port        = "22"
+    protocol    = "tcp"
+    destination_ips = [
+      "0.0.0.0/0",
+    ]
   }
 }
 
