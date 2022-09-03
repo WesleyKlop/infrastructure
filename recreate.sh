92e2f24a0ae13550f8b94b3b992d7c9b54984608
Wesley Klop <wesley19097@gmail.com>
2022-09-03 20:49:35 +0200

    Delete traefik lb if it exists

--- recreate.sh
+++ recreate.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 cd infra
 
 terraform state rm github_repository.infrastructure || true
+hcloud load-balancer delete traefik  
 terraform destroy -auto-approve
 
 terraform import github_repository.infrastructure infrastructure
