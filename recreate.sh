8b2ffca9b8ae97cf6f4ee3ef8ac4bd540a3319b5
Wesley Klop <wesley19097@gmail.com>
2022-09-03 21:07:51 +0200

    Recreate without firewall

--- recreate.sh
+++ recreate.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 cd infra
 
 terraform state rm github_repository.infrastructure || true
-hcloud load-balancer delete traefik  
+hcloud load-balancer delete traefik || true
 terraform destroy -auto-approve
 
 terraform import github_repository.infrastructure infrastructure
 terraform apply -auto-approve
+
+hcloud server ssh control-plane cat .kube/config > ~/.kube/javelin
